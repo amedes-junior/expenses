9674761074ec18b4f4a525615f0710f9ed4bc5b9
Amedes Júnior <amedes@gmail.com>
2021-02-04 14:10:00 -0300

    Expenses -> Fev, 04 de 2021 - Aula 89

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(ExpendesesApp());
 
@@ -10,25 +12,80 @@ class ExpendesesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+        id: 't1',
+        title: 'Novo Tênis de Corrida',
+        value: 310.76,
+        date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Conta de Luz', value: 1211.30, date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Despesas Pessoais'),
-      ),
-      body: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            child: Card(
-              color: Colors.blue,
-              child: Text('Gráfico'),
-              elevation: 5,
+        appBar: AppBar(
+          title: Text('Despesas Pessoais'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              child: Card(
+                color: Colors.blue,
+                child: Text('Gráfico'),
+                elevation: 5,
+              ),
             ),
-          ),
-          Card(child: Text('Lista de Transações'))
-        ],
-      ),
-    );
+            Column(
+              children: _transactions.map((tr) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.purple,
+                            width: 2
+                          )
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          'R\$ ${NumberFormat("#,##0.00", "pt-BR").format(tr.value)}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple
+                          )
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(tr.title,
+                          style: TextStyle(
+                              fontWeight:FontWeight.bold,
+                              fontSize: 16,
+                              color: Colors.black
+                            )),
+                          Text(
+                            DateFormat('d MMM y').format(tr.date),
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey
+                            )
+                          )
+                        ]
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
+          ],
+        ));
   }
 }
